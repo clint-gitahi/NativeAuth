6c127829fb2315f8896236743b811b4e46d3c3d1
clint-gitahi <clingitahi@gmail.com>
2017-05-26 15:07:45 +0300

    final

---
+++ index.android.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+// appId - 1668992890074130
+// appsecret - cd76a1a4f5aef5f02ccd6e6a2bda334a
+
+
+
+// Client ID - 288452546183-urkv4vuo21hatm2l2fdm7ueif3uug0rq.apps.googleusercontent.com
+// Client secret - IZ6w6tschUwPyQJRJsCdW9Xs
+
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app';
+
+AppRegistry.registerComponent('Auth', () => App);
